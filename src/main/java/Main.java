8fe0187a58d245b9fa73a941247858b36abb82f7
Vitaly Nikonorov <nik-vitaly@mail.ru>
2015-10-19 00:36:39 +0300

    command line parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,35 @@
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.apache.commons.cli.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        int port = 8080;
+    private static Options options = new Options();
 
-        Settings.setDirectory( System.getProperty("user.dir") + "/www");
+    static {
+        options.addOption(new Option("r", true, "Root directory"));
+        options.addOption(new Option("c", true, "CPU number"));
+        options.addOption(new Option("p", true, "Port"));
+        options.addOption(new Option("h", true, "Host"));
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+
+
+        CommandLineParser parser = new DefaultParser();
+
+        CommandLine commandLine = parser.parse(options, args);
+
+        String directory = commandLine.getOptionValue("r", "www");
+        String host = commandLine.getOptionValue("h", "0.0.0.0");
+        int port = Integer.parseInt(commandLine.getOptionValue("p", "8080"));
+
+        //Settings.setDirectory( System.getProperty("user.dir") + "/www");
+
+        Settings.setDirectory(directory);
         System.out.print(Settings.getDirectory());
 
         ServerSocket ss = new ServerSocket(port);
